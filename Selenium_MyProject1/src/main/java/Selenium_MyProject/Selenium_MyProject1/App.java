6820b5b140e6411062af7a75eec8988899f16acb
dwivedipriyaranjan <dubeypriyaranjan@gmail.com>
2018-11-20 20:53:20 +0530

    Add files via upload

---
+++ Selenium_MyProject1/src/main/java/Selenium_MyProject/Selenium_MyProject1/App.java
@@ -0,0 +1,13 @@
+package Selenium_MyProject.Selenium_MyProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Selenium_MyProject1/src/test/java/Selenium_MyProject/Selenium_MyProject1/AppTest.java
@@ -0,0 +1,23 @@
+package Selenium_MyProject.Selenium_MyProject1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	@Test
+	
+	public void apptes() {
+		 System.out.println("hi");
+	}
+	
+@Test(enabled=false)	
+	
+	public void apptes1() {
+		 System.out.println("hello");
+	}
+
+}
+    
